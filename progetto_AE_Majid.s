ae6713746c3165849f07bc8eee2150f6efb861d8
saman majid <majidalessio@gmail.com>
2022-08-18 18:29:23 +0200

    Non fa

--- progetto_AE_Majid.s
+++ progetto_AE_Majid.s
@@ -136,8 +136,7 @@ D_ALGORITHM_B:
 D_ALGORITHM_C:
     addi sp sp -4				# Si alloca spazio nella pila per salvare l'indice del ciclo
 	sw t0 0(sp)					# Si salva l'indice nella pila 
-    jal END
-    #jal DECRYPT_ALGORITHM_C
+    jal DECRYPT_ALGORITHM_C
     lw t0 0(sp)					# Si ripristina il valore dell'indice
 	addi sp sp 4				# Si ripristina il valore del puntatore della pila
 	addi t0 t0 1				# Indice++
@@ -425,11 +424,113 @@ end_while_occorrenze:
     lw ra 8(sp)				                 # Si ripristina il valore di ra per uscire correttamente dalla procedura
     addi sp sp 12                            # Ripristino puntatore alla testa della stack
     jr ra
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Procedura che calcola la decrittografia dell'algoritmo ad occorrenze ------------------------------------
 DECRYPT_ALGORITHM_C:
-    
+    addi sp sp -12			# Si alloca spazio per la pila		
+	sw s1 0(sp)				# Si salva il valore di s1
+	sw s0 4(sp)				# Si salva il valore di s0
+	sw ra 8(sp)				# Si salva il valore di ra
+while_reverse_C:
+	lb t1 0(s1)				# t1 = currentChar
+check_position_reverse_C:
+	addi s1 s1 2			# Si incrementa s1 di 2 cosi da poter leggere l'occorrenza siccessiva
+	lb t2 0(s1)				# t2 = terzo carattere, che indica la posizione in cui inserire il carattere corrente
+	lb t3 1(s1)				# t3 = quarto carattere, puo essere uno spazio, un trattino o un altro numero
+	lb t5 2(s1)				# t5 = quarto carattere, se t3 = numero, t5 puo essere uno spazio, un trattino o un altro numero
+	li t6 45
+	beq t3 t6 write_single_position_check_again_reverse_C	# Se t3 = 45 ('-' in ascii), vuol dire che dopo t2 ce' un altra occorrenza, e la posizione corrente e' a singola cifra
+	li t6 32
+	beq t3 t6 write_single_position_next_char_reverse_C	# Se t3 = 32 (' ' in ascii), vuol dire che le occorrenze di questa lettera sono finite, e la posizione corrente e' a singola cifra
+	li t6 0
+	beq t3 t6 write_single_position_end_reverse_C		# Se t3 = 0 ('null' fine stringa in ascii), vuol dire la posizione corrente e' a singola cifra, e il messaggio e' finito
+	li t6 45						# Altrimenti vuol dire che la posizione e' a doppia cifra, e quindi t3 = numero
+	beq t5 t6 write_double_position_check_again_reverse_C	# Se t5 = 45, vuol dire che dopo t2 ce' un altra occorrenza
+	li t6 32
+	beq t5 t6 write_double_position_next_char_reverse_C	# Se t5 = 32, vuol dire che le occorrenze di questa lettera sono finite
+	li t6 0
+	beq t5 t6 write_double_position_end_reverse_C		# Se t5 = 0, vuol dire che dopo questa occorrenza il messaggio e' finito
+write_single_position_check_again_reverse_C:
+	jal ASCII_TO_INT_REVERSE_C
+	j check_position_reverse_C
+write_single_position_next_char_reverse_C:
+	jal ASCII_TO_INT_REVERSE_C
+	addi s1 s1 2			# Si aumenta s1 di due byte perche dobbiamo leggere il carattere successivo
+	j while_reverse_C
+write_single_position_end_reverse_C:
+	jal ASCII_TO_INT_REVERSE_C
+	j end_while_reverse_C
+write_double_position_check_again_reverse_C:
+	jal MERGE_2_ASCII_TO_INT_REVERSE_C
+	addi s1 s1 1			# Si aumenta s1 di un soolo byte perche dobbiamo leggere la prossima occorrenza
+	j check_position_reverse_C
+write_double_position_next_char_reverse_C:
+	jal MERGE_2_ASCII_TO_INT_REVERSE_C
+	addi s1 s1 3			# Si aumenta s1 di 3 byte perche dobbiamo leggere il prossimo carattere
+	j while_reverse_C
+write_double_position_end_reverse_C:
+	jal MERGE_2_ASCII_TO_INT_REVERSE_C
+end_while_reverse_C:
+	lw s1 4(sp)				# Si ripristina il valore di s0, salvandolo pero in s1 (che punta il messaggio criptato)
+	jal MYPLAINTEXT_SIZE			# Chiama la procedura MYPLAINTEXT_LENGTH per calcolare la lunghezza del messaggio in chiaro
+	add s0 s1 s4			# Aggiorniamo l'indirizzo di s0, nel caso dovessimo chiamare un'altra volta l'algoritmo C e avere bisogno di una stringa ancora piu lunga
+	addi s0 s0 1			# Aumentiamo di uno cosi da lasciare spazio per il carattere fine stringa tra messaggio criptato e stringa ausiliaria
+	lw ra 8(sp)				# Si ripristina il valore di ra
+	jr ra				# Si esce dalla procedura
+#Procedura per unire due caratteri asci in un numero intero e salvarlo nel messaggio criptato per l'algoritmo REVERSE_C
+MERGE_2_ASCII_TO_INT_REVERSE_C:
+	addi t2 t2 -48			# t2 = t2 - 49, convertiamo il carattere da ascii a intero
+	addi t3 t3 -48			# t3 = t3 - 48, convertiamo il carattere da ascii a intero
+	li t6 10
+	mul t2 t2 t6			# t2 = t2 * 10, calcoliamo la parte decimale
+	add t4 t2 t3			# t4 = t2 + t3, sommaimo parte decimale con le unita cosi da 'ricostruire' i due caratteri in un singolo numero intero
+	addi t4 t4 -1			# Si sottrae uno perche gli indici per indicare la posizione partono da 0, mentre quelli delle occorrenze da 1
+	add s0 s0 t4			# Si incrementa s0 di t2, ovvero la posizione indicata dall'occorrenza
+	sb t1 0(s0)				# Si salva il carattere corrente in s0
+	lw s0 4(sp)				# Si rispristina l'indirizzo di s0 cosi da poter inserire correttaemnte anche i caratteri succesivi
+	jr ra
+#Procedura per trasformare un numero da ascii a intero e salvarlo nel messaggio criptato per l'algoritmo REVERSE_C
+ASCII_TO_INT_REVERSE_C:
+	addi t2 t2 -49			# t2 = t2 - 49, convertiamo il carattere da ascii a intero
+	add s0 s0 t2			# Si incrementa s0 di t2, ovvero la posizione indicata dall'occorrenza
+	sb t1 0(s0)				# Si salva il carattere corrente in s0
+	lw s0 4(sp)				# Si rispristina l'indirizzo di s0 cosi da poter inserire correttaemnte anche i caratteri succesivi
+	jr ra
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
 # Procedura che calcola la crittografia tramite l'algoritmo dizionario -----------------------------------------------
 DIZIONARIO:
     addi sp sp -4                            # Alloco spazio nella pila per salvare s1
